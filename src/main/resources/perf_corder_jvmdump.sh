081538244a644f4b6822af2428e6a3b510c5e2e0
jaime.casero@gmail.com <jaime.casero@gmail.com>
2016-04-15 17:44:52 +0200

    added sh scripts for easy triggering

---
+++ src/main/resources/perf_corder_jvmdump.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+DIR=$( cd $(dirname $0) ; pwd -P )
+
+if [ -z "$JAVA_HOME" ] ; then
+        JAVA_HOME=`readlink -f \`which java 2>/dev/null\` 2>/dev/null | \
+        sed 's/\/bin\/java//'`
+fi
+
+TOOLSJAR="$JAVA_HOME/lib/tools.jar"
+
+if [ ! -f "$TOOLSJAR" ] ; then
+        echo "$JAVA_HOME seems to be no JDK!" >&2
+        exit 1
+fi
+
+"$JAVA_HOME"/bin/java $JAVA_OPTS -cp "$DIR/sipp-report*.jar:$TOOLSJAR" \
+org.restcomm.perfcorder.collector.VMInfoPrinter "$@"
+exit $?
